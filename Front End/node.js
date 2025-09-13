fe75eae9c6d49c57936d1eec337752130103fed9
kiran <234g1a3367@srit.ac.in>
2025-09-13 10:02:33 +0530

    joke

---
+++ Front End/node.js
@@ -0,0 +1,33 @@
+
+//print statement
+console.log("SRIT")
+
+//Data types in js
+
+num=12;
+str="hi";
+bol=true;
+bigint = 752085208528520520;
+//undefined
+unde;
+null=null;
+
+var,let,const
+
+var name1="srit"
+console.log(name1)
+var name1="hi";
+console.log(name1)
+
+let couse="cse"
+
+let age=20;
+console.log(age)
+age=21;
+console.log(age)
+
+const givename="srivasa rit";
+console.log(givename)
+
+givename="rit";
+console.log(givename)
